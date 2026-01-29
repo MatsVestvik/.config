cdb6ff343e57b80c5191f69cd2b0a18399abd003
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-29 21:36:29 +0100

    working vertical clock and tinted background

--- waybar/verticalclock.sh
+++ waybar/verticalclock.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Get current time in HHMM format
-TIME=$(date +%H%M)
-
-# Print each digit on a new line
-printf "%s\n%s\n:%s\n%s\n" "${TIME:0:1}" "${TIME:1:1}" "${TIME:2:1}" "${TIME:3:1}"
+TIME=$(date +"%H%M")
+printf 'test\ntest'
+printf 'test'
