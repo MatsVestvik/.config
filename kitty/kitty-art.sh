cc1da1aabcca345166a14148ba63975b1fa1f96e
MatsVestvik <mats.o.vestvik@gmail.com>
2026-01-31 00:38:38 +0100

    updated kitty terminal with themes

---
+++ kitty/kitty-art.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo ""
+echo "    /\_/\           ___"
+echo "   ( o.o )         (   )"
+echo "    > ^ <          |   |"
+echo "                   |___|"
+echo ""
+echo "    Welcome to Kitty!"
+echo ""
+exec $SHELL
