77e36344d11ad704b316a185b1962a1b9f0a48ec
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-07 00:01:53 +0100

    removed bloat from nvim

--- nvim/lazy.lua
+++
@@ -1,7 +0,0 @@
-{
-  "mats/netrw-config",
-  config = function()
-    require("config.netrw")()
-  end,
-  lazy = false,
-}
--- nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
-return function()
-  -- Disable bufferline
-  vim.opt.showtabline = 0
-
-  -- Optional: Use alternative buffer navigation
-  vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
-  vim.keymap.set("n", "<leader>bp", ":bprev<CR>", { desc = "Previous buffer" })
-  vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
-end
